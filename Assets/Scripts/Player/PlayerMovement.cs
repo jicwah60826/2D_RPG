bda38d0a65a3ee4111e859068d6fb1ad8c2fff03
DESKTOP-J3LP9F6\jWhisman <jon@pixelgraft.com>
2024-01-26 12:55:19 -0800

    Initial Commit - player WASD movement setup (no animations)

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Config")]
+    [SerializeField] private float speed;
+
+    private PlayerActions actions;
+
+    private Rigidbody2D rb2D;
+
+    private Vector2 moveDirection;
+
+    private void Awake()
+    {
+        actions = new PlayerActions();
+
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        ReadMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb2D.MovePosition(rb2D.position + moveDirection * (speed * Time.fixedDeltaTime));
+    }
+
+    private void ReadMovement()
+    {
+        moveDirection = actions.Movement.Move.ReadValue<Vector2>().normalized;
+    }
+
+    private void OnEnable()
+    {
+        actions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        actions.Disable();
+    }
+}
